1a0a4af18e678498fac211aff0132a1b13b1b078
18310400074 <mabiao123>
2020-01-13 17:55:44 +0800

    添加对话框的使用范例

--- app/src/main/java/com/tools/MainActivity.java
+++ app/src/main/java/com/tools/MainActivity.java
@@ -1,8 +1,11 @@
 package com.tools;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+
+import com.tools.dialog.OpenGiftDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showOpenGift("10.00",true);
+    }
+    /**
+     * 打开中奖的dialog
+     * @param amount 中奖金额
+     * @param isLuck 是否中奖
+     */
+    private void showOpenGift(String amount,boolean isLuck){
+        OpenGiftDialog openGiftDialog = new OpenGiftDialog();
+        Bundle bundle = new Bundle();
+        bundle.putString(OpenGiftDialog.ACTION_AMOUNT,amount);
+        if (isLuck){
+            bundle.putInt(OpenGiftDialog.ACTION_TYPE,OpenGiftDialog.LUCK);
+        }else {
+            bundle.putInt(OpenGiftDialog.ACTION_TYPE,OpenGiftDialog.NO_LUCK);
+        }
+        openGiftDialog.setArguments(bundle);
+        openGiftDialog.setStyle(DialogFragment.STYLE_NO_FRAME,R.style.MyMinDialogWidth);
+        openGiftDialog.show(getSupportFragmentManager(),"对话框");
     }
 }
